f255fa5f553cad5210c31b6f2138c360ea579454
paket3 <alexisgaete@gmail.com>
2020-02-18 20:08:01 -0300

    actualizacion respaldo configuraciones 20200218

--- challenge.sh
+++ challenge.sh
@@ -10,5 +10,11 @@ while IFS= read -r line
 do
 	mkdir -p $working_dir/backups/$line
 	#rm -rf $working_dir/backups/$line/running-config
+	echo "Copiando configuraciones desde $line"
 	scp -o StrictHostKeyChecking=no $line:/flash/running-config $working_dir/backups/$line/running-config_$date
 done < "$working_dir/switches.txt"
+
+cd $working_dir
+git add -A .
+git commit -m "actualizacion respaldo configuraciones $date" -a
+git push
